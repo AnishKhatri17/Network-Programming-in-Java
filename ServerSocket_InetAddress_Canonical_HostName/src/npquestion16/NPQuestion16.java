9634e0cdf723f523d57f3c91cac9a3887c41d586
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-19 10:44:45 +0545

    ServerSocket program to find InetAddress, Canonical HostName, and HostName

---
+++ ServerSocket_InetAddress_Canonical_HostName/src/npquestion16/NPQuestion16.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package npquestion16;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.*;
+
+public class NPQuestion16 {
+
+    public static void main(String[] args) {
+    
+        try{
+            ServerSocket serverSocket = new ServerSocket(0);
+            System.out.println("\n\nServer is running on port :"+serverSocket.getLocalPort());
+            
+            //print the inetAddress Information
+            InetAddress inetAddress = serverSocket.getInetAddress();
+            System.out.println("\nInetAddress is : "+inetAddress.getHostAddress());
+            System.out.println("\nCanonical Host Name is : "+inetAddress.getCanonicalHostName());
+            System.out.println("\nHost Name is : "+inetAddress.getHostName());
+
+            serverSocket.close();
+        }
+        
+        catch(IOException ex)
+        {
+            System.out.println("Server Exception"+ex.getMessage());
+        }
+    }
+    
+}
